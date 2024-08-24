afc9a1f042de2080384d7c4eaa66643a54cc7912
Aleksandr <ShirshovAleksandrWA@yandex.ru>
2024-08-24 17:12:23 +0300

    Add validate datefield

--- deposit/deposit_calculator/serializers.py
+++ deposit/deposit_calculator/serializers.py
@@ -1,8 +1,30 @@
+from datetime import datetime, date
 from rest_framework import serializers
 from .models import Deposit
 
 
+class DateFormatField(serializers.Field):
+    def to_representation(self, value):
+        if value:
+            return value.strftime('%d.%m.%Y')
+        return ''
+
+    def to_internal_value(self, data):
+        if data:
+            try:
+                dt = datetime.strptime(data, '%d.%m.%Y')
+                if dt > datetime.today():
+                    return dt
+                else:
+                    raise serializers.ValidationError('Дата не может быть ранее текущей даты')
+            except ValueError:
+                raise serializers.ValidationError('Дата должна быть в формате dd.mm.yyyy')
+        return None
+
+
 class DepositSerializer(serializers.ModelSerializer):
+    date = DateFormatField()
+
     class Meta:
         model = Deposit
         fields = '__all__'
--- deposit/deposit_calculator/validators.py
+++
@@ -1,6 +0,0 @@
-from django.core.exceptions import ValidationError
-from django.utils import timezone
-
-def validate_not_before_today(value):
-    if value < timezone.now().date():
-        raise ValidationError('Дата не может быть раньше текущей даты.')
