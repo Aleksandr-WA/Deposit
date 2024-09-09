c19dae9ce03b142abb97ecf7bc50d04cb1c07cf2
Aleksandr-WA <ShirshovAleksandrWA@yandex.ru>
2024-09-09 17:39:05 +0300

    refactoring views

--- deposit/deposit_calculator/views.py
+++ deposit/deposit_calculator/views.py
@@ -8,23 +8,23 @@ from .serializers import DepositSerializer
 class DepositCalculator(APIView):
     def post(self, request):
         serializer = DepositSerializer(data=request.data)
-        if serializer.is_valid():
-            date = serializer.validated_data["date"]
-            periods = serializer.validated_data["periods"]
-            amount = serializer.validated_data["amount"]
-            rate = serializer.validated_data["rate"]
-            serializer.validated_data["calculated_value"] = self.calculate_deposit(
-                date, amount, rate, periods
-            )
-            serializer.save()
-            return Response(
-                serializer.validated_data["calculated_value"], status=status.HTTP_200_OK
-            )
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.validated_data["calculated_value"] = self.calculate_deposit(
+            serializer
+        )
+        serializer.save()
+        return Response(
+            serializer.validated_data["calculated_value"],
+            status=status.HTTP_200_OK,
+        )
 
     @staticmethod
-    def calculate_deposit(date, amount, rate, periods):
+    def calculate_deposit(serializer):
         future_value = {}
+        date = serializer.validated_data["date"]
+        periods = serializer.validated_data["periods"]
+        amount = serializer.validated_data["amount"]
+        rate = serializer.validated_data["rate"]
         monthly_rate = rate / 12 / 100
         for month in range(1, periods + 1):
             key_date = (date + relativedelta(months=month)).strftime("%d.%m.%Y")
