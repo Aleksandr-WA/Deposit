49977c25599088d53950bdc4057a76a29ca1cfde
Aleksandr-WA <ShirshovAleksandrWA@yandex.ru>
2024-09-09 17:26:38 +0300

    refactoring urls

--- deposit/deposit/urls.py
+++ deposit/deposit/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/deposit/", include("deposit_calculator.urls")),
+    path("api/v1/deposit/", include("deposit_calculator.urls")),
 ]
